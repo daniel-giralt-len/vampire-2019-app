21c033214481e2ff80594bab99820f0ed13c05d5
Daniel Giralt-Len <daniel.giralt.len@gmail.com>
2019-10-18 21:28:17 +0200

    draw resources & damages

--- src/client/apps/status.js
+++ src/client/apps/status.js
@@ -8,42 +8,61 @@ const StyledApp = styled.main`
     overflow-y: scroll;
 `
 
-const Circle = styled.div`
+const SectionTitle = styled.h2`
+    color: ${colors.red1}
+    width: 100%;
+    border-bottom: 3px solid ${colors.red1}
+    text-align: center;
+    padding-bottom: 3px;
+    margin-bottom: 5px;
+`
+
+const Square = styled.div`
+    font-size: 4px;
+    text-align: center;
     display: inline-block;
     ${({ full }) => full ? `background-color: ${colors.black1};` : ''}
     ${({ size = '5px' }) => {
         return `
         width: ${size};
         height: ${size};
-        border-radius: ${size};
         border: 1px solid ${colors.black1}
         `
     }}
 `
 
-const Square = styled.div`
-    ${({ full }) => full ? `background-color: ${colors.black1};` : ''}
-    ${({ size = '5px' }) => {
-        return `
-        width: ${size};
-        height: ${size};
-        border: 1px solid ${colors.black1}
-        `
-    }}
+const Circle = styled(Square)`
+    ${({ size = '5px' }) => `border-radius: ${size};`}
 `
 
-const Damage = ({ type }) => {
-    const text = {
-        none: '',
-        superficial: '/',
-        aggravated: 'X',
-    }[type]
-    return <Square>{text}</Square>
+const Damage = ({ label, superficial, aggravated }) => {
+    console.log(superficial,aggravated)
+    return (<StyledResource>
+        <div>{label}</div>
+        <div>{
+            Array(10)
+                .fill()
+                .map((_, index) => {
+                    let text = ' '
+                    if(index < aggravated){
+                        text = 'X'
+                    }else if(index < aggravated + superficial){
+                        text = '/'
+                    }
+                    return (<Square>{text}</Square>)
+                })
+            }
+        </div>
+    </StyledResource>)
 }
 
-const StatTypeColumns = styled.div`
+const Columns = styled.ul`
     display: grid;
-    grid-template-columns: repeat(3,1fr);
+    grid-template-columns: repeat(${({ amount = 3 }) => amount},1fr);
+    list-style: none;
+`
+
+const StatTypeColumns = styled(Columns)`
     grid-template-areas: 
         "physical social mental";
 `
@@ -55,30 +74,32 @@ const StatColumn = styled.ul`
     grid-area: ${({ type }) => type};
 `
 
-const SectionTitle = styled.h2`
-    color: ${colors.red1}
-    width: 100%;
-    border-bottom: 3px solid ${colors.red1}
-    text-align: center;
-    padding-bottom: 3px;
-    margin-bottom: 5px;
+const StyledResource = styled.div`
+    display: flex;
+    justify-content: space-between;
 `
 
-const StyledStat = styled.li`
-
-`
-
-const Stat = ({label, amount}) => {
- return (
-    <StyledStat>
-        <div>{label}</div>
-        <div>{Array(5).fill().map((_, index) => (<Circle full={index < amount} />))}</div>
-    </StyledStat>
- )
+const Resource = ({ label, amount, maxAmount = 10 }) => {
+    return (
+        <StyledResource>
+            <div>{label}</div>
+            <div>{Array(maxAmount).fill().map((_, index) => (<Square full={index < amount} />))}</div>
+        </StyledResource>
+    )
 }
 
-const StatsSection = ({stats}) => {
-        console.log(stats)
+const StyledStat = styled.li``
+
+const Stat = ({ label, amount }) => {
+    return (
+        <StyledStat>
+            <div>{label}</div>
+            <div>{Array(5).fill().map((_, index) => (<Circle full={index < amount} />))}</div>
+        </StyledStat>
+    )
+}
+
+const StatsSection = ({ stats }) => {
     return (<StatTypeColumns>
         {Object.keys(stats).map(type => {
             return <StatColumn
@@ -99,15 +120,35 @@ const StatusApp = () => {
     }, [])
 
     if (!data) return ''
-    console.log(data)
-    const { general, damage, stats } = data
+    const { general, anxiety, humanity, damage, stats } = data
     const { attributes, skills, disciplines } = stats
     return (<StyledApp>
+        <SectionTitle>Resources</SectionTitle>
+        <Columns amount={2}>
+            <Resource label='anxiety' amount={anxiety} maxAmount={5} />
+            <Resource label='humanity' amount={humanity} />
+            {
+                Object.entries(damage)
+                    .map(([label, { superficial, aggravated }]) => (
+                        <Damage
+                            aggravated={aggravated}
+                            label={label}
+                            superficial={superficial}
+                        />
+                    ))
+            }
+        </Columns>
+        <SectionTitle>Disciplines</SectionTitle>
+        <Columns>
+            {
+                Object.entries(disciplines)
+                    .map(([label, amount]) => (<Stat label={label} amount={amount} />))
+            }
+        </Columns>
         <SectionTitle>Attributes</SectionTitle>
         <StatsSection stats={attributes} />
         <SectionTitle>Skills</SectionTitle>
         <StatsSection stats={skills} />
-        <SectionTitle>Disciplines</SectionTitle>
     </StyledApp>)
 }
 
