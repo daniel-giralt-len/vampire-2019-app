b2880d60c3b9c10559835f2a9ba30499955541ef
daniel-giralt-len <daniel.giralt.len@gmail.com>
2019-10-24 22:34:07 +0200

    refactor: add header&navigator to components folder

---
+++ src/client/components/header.js
@@ -0,0 +1,79 @@
+import React, { useContext, useState } from 'react'
+import styled from 'styled-components'
+import Cog from '../svgs/cog'
+import translate, { availableLanguages, Language } from '../translate-component'
+
+const StyledHeader = styled.header`
+    background-color: ${({ theme }) => theme.red1};
+    color: ${({ theme }) => theme.white1};
+    padding: 10px 0px;
+    font-size: 2em;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    position: relative;
+`
+
+const ConfigMenu = styled.div`
+  background-color: ${({ theme }) => theme.blue1};
+  color: ${({ theme }) => theme.white1};
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
+  right: 0;
+  font-size: 2em;
+  min-width: 250px;
+  box-shadow: -2px 2px 4px black;
+`
+
+const LanguageButton = styled.button`
+  font-size: 1em;
+  margin-right: 5px;
+  color: ${({ theme }) => theme.white1};
+  &:disabled{
+    color: ${({ theme }) => theme.grey1};
+  }
+`
+const Toggle = styled.button`
+  margin-top: 1em;
+  font-size: 1em;
+  color: ${({ theme }) => theme.background};
+  background-color: ${({ theme }) => theme.font};
+`
+
+const SvgContainer = styled.div`display:flex;`
+
+const Header = ({ onThemeToggle, onLanguageChange, t, theme }) => {
+  const [isConfigOpen, setConfig] = useState(false)
+  const toggleConfigMenu = () => setConfig(!isConfigOpen)
+  const language = useContext(Language)
+  return (
+    <div>
+      <StyledHeader>
+        <div>Rainy</div>
+        <div>13:52</div>
+        <SvgContainer onClick={toggleConfigMenu} ><Cog width='1.2em' height='1.2em' /></SvgContainer>
+      </StyledHeader>
+      {isConfigOpen && <ConfigMenu>
+        {
+          availableLanguages
+            .map(lang => (<LanguageButton
+              key={lang}
+              disabled={lang === language}
+              onClick={() => onLanguageChange(lang)}
+            >
+              {t(`header.${lang}`)}
+            </LanguageButton>))
+        }
+        <Toggle onClick={onThemeToggle}>
+          {theme === 'light' ? t('header.dark') : t('header.light')}
+        </Toggle>
+      </ConfigMenu>
+      }
+    </div>)
+}
+
+export default translate(Header)
---
+++ src/client/components/navigator.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import GroupSvg from '../svgs/group'
+import NewspaperSvg from '../svgs/newspaper'
+import PersonSvg from '../svgs/person'
+import RelationshipsSvg from '../svgs/relationships'
+import SpeechBubleSvg from '../svgs/speech-bubble'
+import WorldMapSvg from '../svgs/world-map'
+import Cross from '../svgs/cross'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import translate from '../translate-component'
+
+const Apps = styled.ul`
+    z-index: 1;
+
+    display: grid;
+    grid-template-columns: repeat(${({ amount }) => amount},1fr);
+    column-gap: 5px;
+    padding: 10px 3px;
+
+    bottom: 0;
+    overflow-x: scroll;
+    width: 100%;
+
+    background-color: ${({ theme }) => theme.black1};
+    color: ${({ theme }) => theme.white1}
+    position:fixed;
+    box-shadow: 0px -2px 20px black;
+
+    transition: transform 0.5s;
+    transform: scaleY(${({ hidden }) => hidden ? 0 : 1});
+    transform-origin: bottom;
+`
+
+const StyledAppLink = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-direction: center;
+    align-items: center;
+    text-decoration: none;
+    font-size: 2em;
+`
+
+const AppSvg = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const AppLink = translate(({ Svg, link, label, onClick, t }) => {
+    const WrapperComponent = link ? (Link) : ('div')
+    return (
+        <WrapperComponent
+            onClick={onClick}
+            to={link}
+        >
+            <StyledAppLink>
+                <AppSvg>
+                    <Svg width='100px' height='100px' />
+                </AppSvg>
+                {label && t(`navigator.${label}`)}
+            </StyledAppLink>
+        </WrapperComponent>
+    )
+})
+
+const apps = [
+    { label: 'map', Svg: WorldMapSvg, link: '/map' },
+    { label: 'messenger', Svg: SpeechBubleSvg, link: '/messenger' },
+    { label: 'couterie', Svg: GroupSvg, link: '/couterie' },
+    { label: 'news', Svg: NewspaperSvg, link: '/news' },
+    { label: 'status', Svg: PersonSvg, link: '/status' },
+    { label: 'bonds', Svg: RelationshipsSvg, link: '/relationships' },
+]
+
+const Navigator = ({ hidden, onClick, onCloseApps }) => {
+    return (
+        <Apps amount={apps.length + 1} hidden={hidden}>
+            {apps.map(({ label, Svg, link }) => (
+                <AppLink
+                    key={label}
+                    link={link}
+                    label={label}
+                    Svg={Svg}
+                    onClick={onClick}
+                />
+            ))}
+            <AppLink
+                Svg={Cross}
+                onClick={onCloseApps}
+            />
+        </Apps>
+    )
+}
+
+export default Navigator
