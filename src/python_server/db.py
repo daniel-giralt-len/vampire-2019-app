6d495362069970ac50a299af5d9e51f80aab46b5
daniel-giralt-len <daniel.giralt.len@gmail.com>
2019-10-30 19:27:34 +0100

    fix: don't initialize couterie db

--- src/python_server/db.py
+++ src/python_server/db.py
@@ -2,6 +2,5 @@ from tinydb import TinyDB, Query
 
 _db = TinyDB('database.json', indent=4)
 maps = _db.table('maps')
-couterie = _db.table('couterie')
 characters = _db.table('characters')
 players = _db.table('players')
