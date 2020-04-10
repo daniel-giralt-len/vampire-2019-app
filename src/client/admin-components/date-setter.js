d6dbc7a59157185bf042d9182f5581ddab87096f
daniel-giralt-len <daniel.giralt.len@gmail.com>
2020-04-10 19:22:20 +0200

    add date setter save button

--- src/client/admin-components/date-setter.js
+++ src/client/admin-components/date-setter.js
@@ -1,37 +1,49 @@
 import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
+import translate from '../translate-component'
 import API from '../api'
 
-const DateSetter = ({ hidden, onClick, onCloseApps }) => {
+const DateSetter = ({ t }) => {
   const [hours, setHours] = useState(null)
   const [minutes, setMinutes] = useState(null)
+  const [epoch, setEpoch] = useState(null)
+
+  const saveDate = () => {
+    const newDate = new Date(epoch)
+    newDate.setHours(hours)
+    newDate.setMinutes(minutes)
+    API.setDate(newDate.getTime())
+  }
 
   useEffect(() => {
     API.getEpoch()
       .then(({epoch}) => {
         const date = new Date(epoch)
+        setEpoch(epoch)
         setHours(date.getHours())
         setMinutes(date.getMinutes())
       })
   }, [])
 
   return (
-    <div>
+    <React.Fragment>
       <input 
         type='number' 
-        name='hours'
+        name={t('admin.date.hours.label')}
         value={hours ? hours : 0}
         onChange={event => setHours(event.target.value)}
       />
       :
       <input 
         type='number' 
-        name='minutes'
+        name={t('admin.date.minutes.label')}
         value={minutes ? minutes : 0}
         onChange={event => setMinutes(event.target.value)}
       />
-    </div>
+      <button onClick={saveDate} >
+        {t('admin.date.save')}
+      </button>
+    </React.Fragment>
   )
 }
 
-export default DateSetter
+export default translate(DateSetter)
--- src/client/api/narrator-requests.js
+++ src/client/api/narrator-requests.js
@@ -37,6 +37,16 @@ const narratorRequests = {
             })
         }
     ).then(res => res.json())
+  },
+  setDate: epoch => {
+    return fetch(
+      `${baseURL}/date`,
+      {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ epoch: epoch })
+      }
+    ).then(res => res.json())
   }
 }
 
